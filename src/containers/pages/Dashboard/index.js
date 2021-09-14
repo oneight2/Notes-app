6ce9d89b7495b4fb1f3188ace22ec01768a7d15c
oneight2 <syarif.cibatu@gmail.com>
2021-09-14 07:59:39 +0700

    dasboard

--- src/containers/pages/Dashboard/index.js
+++ src/containers/pages/Dashboard/index.js
@@ -4,7 +4,45 @@ export default class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1>Dashboard</h1>
+        <div className="flex justify-center">
+          <div className="flex-1">
+            <div class="card lg:card-side bordered shadow-lg my-10">
+              <div class="card-body">
+                <h2 class="card-title">Notes </h2>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Title</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="title"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">info</span>
+                  </label>
+                  <textarea
+                    className="textarea h-24 textarea-bordered textarea-info"
+                    placeholder="Bio"
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex1">
+            <div className="divider">OR</div>
+          </div>
+          <div className="flex1">
+            <div className="card shadow-lg mx-5">
+              <div className="card-body">
+                <h2 className="card-title">no border with shadow</h2>
+                <p>Rerum reiciendis beatae tenetur excepturi</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
